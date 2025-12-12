f5437786c936c98544228e15901371445bbcce53
ehattab <hattab.elies@gmail.com>
2025-12-12 16:45:18 +0100

    spawn

--- parsing/cub3d.h
+++ parsing/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: ehattab <ehattab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 18:49:44 by tony              #+#    #+#             */
-/*   Updated: 2025/12/12 16:32:07 by ehattab          ###   ########.fr       */
+/*   Updated: 2025/12/12 16:44:28 by ehattab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,8 +73,30 @@ typedef	struct s_map
 	char	p;
 }			t_map;
 
+// typedef	struct s_parsing
+// {
+// 	char	**map_copy;
+// 	int		width_copy;
+// };
 
+
+// void	check_fd(int fd);
+// int		check_file(char *argv);
+// int		count_lines(char *argv);
+// void	print_map(char **argv);
+// int		check_space(char *str);
+// int		check_big_len(char **map);
+// char	**map_scan(char **map, char *argv);
+// void	ft_error(char *str, char **map);
+// void	free_map(char **map);
+// int		check_nums(char *str);
+// void	check_instruction(char **beforemap);
+// void	check_path(char **beforemap);
+// void	check_double(char **map_copy);
 void	search_position(char **map_copy, t_map *map);
+// void	error(int x, int y);
+// int		is_alpha(char *str);
+// int		check_xpm(char *str);
 
 //Game
 void	init_game(t_game *game, t_map *map);
--- parsing/utils3.c
+++ parsing/utils3.c
@@ -6,8 +6,38 @@
 /*   By: ehattab <ehattab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 17:53:27 by toroman           #+#    #+#             */
-/*   Updated: 2025/12/12 16:31:52 by ehattab          ###   ########.fr       */
+/*   Updated: 2025/12/12 16:43:54 by ehattab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+// int	is_alpha(char *str)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while(str[i])
+// 	{
+// 		if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+// 			return (1);
+// 		if (str[i] == '.')
+// 			return (1);
+// 		i++;
+// 	}
+// 	return (0);
+// }
+
+// int	check_xpm(char *str)
+// {
+// 	int i;
+
+// 	i = 0;
+	
+// 	while(str[i])
+// 	{
+// 		if (ft_strncmp(str + ft_strlen(str) - 4, "xpm", 4))
+// 			return (1);
+// 	}
+// 	return (0);
+// }
